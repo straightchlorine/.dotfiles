e5276bfbbf9e1c344df82d4ae781c6d6d9fa6c37
Piotr <piotrlis555@gmail.com>
2022-06-28 23:12:52 +0200

    just changed the theme folder

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -46,7 +46,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 
-local theme_path = string.format("%s/.config/awesome/themes/%s/theme.lua", os.getenv("HOME"), "default")
+local theme_path = string.format("%s/.config/awesome/themes/%s/theme.lua", os.getenv("HOME"), "initial")
 beautiful.init(theme_path)
 for s = 1, screen.count() do
 	gears.wallpaper.maximized(beautiful.wallpaper, s, true)
