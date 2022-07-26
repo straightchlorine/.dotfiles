66722e3fa6391fa0dc3bae9ec45d5850cba63e42
Piotr <piotrlis555@gmail.com>
2022-07-26 22:48:40 +0200

    inital setup of calendar widget

--- .config/awesome/aesthetics/calendar.lua
+++ .config/awesome/aesthetics/calendar.lua
@@ -1,3 +1,13 @@
 local wibox = require('wibox')
 local gears = require('gears')
 local awful = require('awful')
+
+-- initial future widget setup
+calendar_widget = wibox.widget {
+    date        = os.date('*t'),
+    font        = "sans 8",
+    long_weekdays = false,
+    spacing     = dpi(3),
+    fn_enabled  = decorate_cell,
+    widget      = wibox.widget.calendar.month
+}
