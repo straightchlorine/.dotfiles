d0f47d68e479483574b4d23abf1a147d23146c7b
Piotr <piotrlis555@gmail.com>
2022-07-27 21:51:06 +0200

    of no importance

--- .config/awesome/aesthetics/calendar.lua
+++ .config/awesome/aesthetics/calendar.lua
@@ -3,6 +3,7 @@ local gears = require('gears')
 local awful = require('awful')
 
 -- initial future widget setup
+-- maybe try to turn in into dock of sorts?
 calendar_widget = wibox.widget {
     date        = os.date('*t'),
     font        = "sans 8",
